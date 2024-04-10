e62147c0d95934e0e739d2683aea625dcdc88ac9
shiva00007 <shivas91919@gmail.com>
2024-04-10 21:29:30 +0530

    commit done

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,10 +74,10 @@ let swiperFeatured = new Swiper('.featured_swiper', {
     slidesPerView:'auto',
     centeredSlides:'auto',
 
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    //   },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     
 
     breakpoints:{
@@ -102,15 +102,68 @@ let swiperNew = new Swiper('.new_swiper', {
   });
 
 /*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestimonial = new Swiper('.testimonial__swiper', {
+    loop: true,
+    spaceBetween:16,
+    grabCursor:true,
+    slidesPerView:'auto',
+    centeredSlides:'auto',
 
+    breakpoints:{
+        1150:{
+            slidesPerView:3,
+            centeredSlides:false,
+        }
+    }
+  });
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
 
-
+    this.scrollY >=350 ? scrollUp.classList.add('show-scroll')
+    :scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
 
+// (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => document.body.classList.contains(darkTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    // user needs to save theme
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin : 'top',
+    distance : '60px',
+    duration : 2500,
+    delay :400 ,
+    //rest :true animation repeat
+})
+sr.reveal('.home__data, .featuerd__container, .new__cotainer,.testimonial__container, .footer')
+sr.reveal('.home__images',{delay : 600})
+sr.reveal('.services__container',{interval:100})
+sr.reveal('.discount__data',{origin:'left'})
+sr.reveal('.discount__images',{origin:'right'})
